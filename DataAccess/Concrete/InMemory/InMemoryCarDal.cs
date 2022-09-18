2a688174700a6f690e1fc7f716228ffa2cad79dd
baldk <baldk@DESKTOP-P4383FE>
2022-09-19 00:00:30 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>()
+            {
+                new Car{CarId=1,BrandId=1,ColorId=1,ModelYear=1996,DailyPrice=300,Description="Araç 80000 km'de."},
+                new Car{CarId=2,BrandId=2,ColorId=2,ModelYear=2022,DailyPrice=2000,Description="Araç 2000 km'de" },
+                new Car{CarId=3,BrandId=3,ColorId=3,ModelYear=2014,DailyPrice=900,Description="Araç 12000 km'de"},
+                new Car{CarId=4,BrandId=4,ColorId=4,ModelYear=2008,DailyPrice=700,Description="Araç 25000 km'de"}            
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetByldBrand(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public List<Car> GetByldColor(int colorId)
+        {
+            return _cars.Where(c => c.ColorId == colorId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
